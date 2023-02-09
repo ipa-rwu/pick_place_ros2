1eacefa9d1ab5ed79c26968f7f2cc0bd592fbadb
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-02-10 00:21:45 +0100

    fix get ik frame transfer

--- pick_place_app/include/pick_place_app/skills/utils.h
+++ pick_place_app/include/pick_place_app/skills/utils.h
@@ -12,6 +12,7 @@
 #include <boost/any.hpp>
 #include <yaml-cpp/yaml.h>
 #include <moveit_msgs/msg/constraints.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
 
 namespace robot_skills
 {
@@ -19,17 +20,18 @@ namespace utils
 {
 const std::string CURRENT_PKG = "pick_place_app";
 
-bool getRobotTipForFrame(const geometry_msgs::msg::PoseStamped ik_frame,
-                         const planning_scene::PlanningSceneConstPtr& scene,
+bool getRobotTipForFrame(const planning_scene::PlanningSceneConstPtr& scene,
                          const moveit::core::JointModelGroup* jmg,
                          const moveit::core::LinkModel*& robot_link,
-                         Eigen::Isometry3d& tip_in_global_frame);
+                         Eigen::Isometry3d& tip_in_global_frame,
+                         const std::string ik_frame_id = "");
 
 template <typename T>
 T getValueFromYaml(const YAML::Node& node, const std::string& key);
 
 void loadPathConstraintsFromYaml(const std::string path_constraints_yaml,
                                  moveit_msgs::msg::Constraints& path_constraints);
+
 }  // namespace utils
 
 }  // namespace robot_skills
--- pick_place_app/src/skills/utils.cpp
+++ pick_place_app/src/skills/utils.cpp
@@ -7,11 +7,10 @@ namespace utils
 {
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("RobotSkills_utils");
 
-bool getRobotTipForFrame(const geometry_msgs::msg::PoseStamped ik_frame,
-                         const planning_scene::PlanningSceneConstPtr& scene,
+bool getRobotTipForFrame(const planning_scene::PlanningSceneConstPtr& scene,
                          const moveit::core::JointModelGroup* jmg,
                          const moveit::core::LinkModel*& robot_link,
-                         Eigen::Isometry3d& tip_in_global_frame)
+                         Eigen::Isometry3d& tip_in_global_frame, const std::string ik_frame_id)
 {
   auto get_tip = [&jmg]() -> const moveit::core::LinkModel* {
     // determine IK frame from group
@@ -25,7 +24,7 @@ bool getRobotTipForFrame(const geometry_msgs::msg::PoseStamped ik_frame,
     return tips[0];
   };
 
-  if (ik_frame.header.frame_id.empty())
+  if (ik_frame_id.empty())
   {
     robot_link = get_tip();
     if (!robot_link)
@@ -33,22 +32,19 @@ bool getRobotTipForFrame(const geometry_msgs::msg::PoseStamped ik_frame,
       RCLCPP_ERROR(LOGGER, "missing ik_frame");
       return false;
     }
+    // transfer of tip (tip pose) in planning frame
     tip_in_global_frame = scene->getCurrentState().getGlobalLinkTransform(robot_link);
-    RCLCPP_DEBUG(LOGGER, "tip_in_global_frame: %s",
-                 geometry_msgs::msg::to_yaml(tf2::toMsg(tip_in_global_frame)).c_str());
   }
   else
   {
-    tf2::fromMsg(ik_frame.pose, tip_in_global_frame);
-
     robot_link = nullptr;
     bool found = false;
-    auto ref_frame =
-        scene->getCurrentState().getFrameInfo(ik_frame.header.frame_id, robot_link, found);
-    if (!found && !ik_frame.header.frame_id.empty())
+    // if found, get transfer ik_frame_id (ik_frame pose) in planning frame
+    auto ref_frame = scene->getCurrentState().getFrameInfo(ik_frame_id, robot_link, found);
+    if (!found && !ik_frame_id.empty())
     {
       std::stringstream ss;
-      ss << "ik_frame specified in unknown frame '" << ik_frame.header.frame_id << "'";
+      ss << "ik_frame specified in unknown frame '" << ik_frame_id << "'";
       RCLCPP_ERROR(LOGGER, "%s", ss.str().c_str());
       return false;
     }
@@ -60,12 +56,15 @@ bool getRobotTipForFrame(const geometry_msgs::msg::PoseStamped ik_frame,
 
       return false;
     }
-    else if (!found)
-    {  // use robot link's frame as reference by default
-      ref_frame = scene->getCurrentState().getGlobalLinkTransform(robot_link);
-    }
 
-    tip_in_global_frame = ref_frame * tip_in_global_frame;
+    if (found)
+    {  // use robot link's frame as reference by default
+      tip_in_global_frame = ref_frame;
+    }
+    else
+    {
+      tip_in_global_frame = scene->getCurrentState().getGlobalLinkTransform(robot_link);
+    }
   }
 
   return true;
