f573a82a71bcc92c8e4d80e437f630dea09de9d6
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2023-04-20 12:16:46 +0200

    format

--- pick_place_app/src/main_pick_place_task.cpp
+++ pick_place_app/src/main_pick_place_task.cpp
@@ -10,43 +10,6 @@
 using namespace std::chrono_literals;
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("pick_place_demo");
 
-moveit_msgs::msg::CollisionObject createTable(const std::string& table_name,
-                                              const geometry_msgs::msg::Pose& pose)
-{
-  moveit_msgs::msg::CollisionObject object;
-  object.id = table_name;
-  object.header.frame_id = "world";
-  object.primitives.resize(1);
-  object.primitives[0].type = shape_msgs::msg::SolidPrimitive::BOX;
-  object.primitives[0].dimensions.resize(
-      geometric_shapes::solidPrimitiveDimCount<shape_msgs::msg::SolidPrimitive::BOX>());
-  object.primitives[0].dimensions.at(shape_msgs::msg::SolidPrimitive::BOX_X) = 0.2;
-  object.primitives[0].dimensions.at(shape_msgs::msg::SolidPrimitive::BOX_Y) = 0.2;
-  object.primitives[0].dimensions.at(shape_msgs::msg::SolidPrimitive::BOX_Z) = 0.25;
-  object.primitive_poses.push_back(pose);
-  object.primitive_poses.back().position.z +=
-      0.5 * object.primitives[0].dimensions.at(
-                shape_msgs::msg::SolidPrimitive::BOX_Z);  // align surface with world
-  return object;
-}
-
-moveit_msgs::msg::CollisionObject createObject(const std::string object_name,
-                                               const std::string frame_id,
-                                               const geometry_msgs::msg::Pose& pose)
-{
-  moveit_msgs::msg::CollisionObject object;
-  object.id = object_name;
-  object.header.frame_id = frame_id;
-  object.primitives.resize(1);
-  object.primitives[0].type = shape_msgs::msg::SolidPrimitive::CYLINDER;
-  object.primitives[0].dimensions.resize(
-      geometric_shapes::solidPrimitiveDimCount<shape_msgs::msg::SolidPrimitive::CYLINDER>());
-  object.primitives[0].dimensions.at(shape_msgs::msg::SolidPrimitive::CYLINDER_HEIGHT) = 0.1;
-  object.primitives[0].dimensions.at(shape_msgs::msg::SolidPrimitive::CYLINDER_RADIUS) = 0.02;
-  object.pose = pose;
-  return object;
-}
-
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
@@ -60,7 +23,6 @@ int main(int argc, char** argv)
     executor.spin();
     executor.remove_node(node->get_node_base_interface());
   });
-  spin_thread->detach();
 
   robot_application::PickPlaceTask::Parameters pick_place_parameters;
   pick_place_parameters.loadParameters(node);
@@ -68,6 +30,7 @@ int main(int argc, char** argv)
   robot_application::PickPlaceTask pick_place_task(node, pick_place_parameters);
 
   pick_place_task.executeTask();
+  spin_thread->join();
 
   rclcpp::shutdown();
   return 0;
